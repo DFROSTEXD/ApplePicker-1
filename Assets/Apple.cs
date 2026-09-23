de5b4ac63cc2399697d34a986ae02f64f714f1fc
DFROSTEXD <willh6789@gmail.com>
2026-09-23 09:19:57 -0400

    Added text and scripts for keeping score

---
+++ Assets/Apple.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour {                                    // a
+    public static float     bottomY = -20f;                             // b
+
+    void Update () {
+        if ( transform.position.y < bottomY ) {
+            Destroy( this.gameObject );                                 // c
+
+            // Get a reference to the ApplePicker component of Main Camera
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();           // b
+            // Call the public AppleMissed() method of apScript
+            apScript.AppleMissed();
+        }
+    }
+}
